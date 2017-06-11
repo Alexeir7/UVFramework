3ea4a412201238af65d88ec3912b3be60fa0b9f8
alexe <alexe@Alexei-PC>
2017-06-11 10:35:08 -0600

    modificar ventanas para estandarizacion

--- src/uvframework/controllers/LoginViewController.java
+++ src/uvframework/controllers/LoginViewController.java
@@ -6,18 +6,10 @@
 package uvframework.controllers;
 
 import java.net.URL;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import uvframework.models.UsuariosModel;
 import uvframework.models.entities.UsuarioEntity;
 
